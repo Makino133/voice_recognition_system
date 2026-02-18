282fab98ac9da4933911ed6f2d0204134931f691
robot-Yang <absorbraver@gmail.com>
2026-02-18 11:02:47 +0900

    Launch partially implemented

---
+++ vint_ros/vint_ros/Edge_Labeling_node.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+import rclpy
+import numpy as np
+import math
+from rclpy.node import Node
+from visualization_msgs.msg import Marker
+from std_msgs.msg import String
+
+
+class EdgeLabeling(Node):
+    def __init__(self):
+        super().__init__("Edge_Labeling")
+
+        self.edge_pub = self.create_publisher(String, "/edge_list", 10)
+
+        self.state = {
+            "robot": {"pos": None, "quat": None, "size": None},
+            "table": {"pos": None, "quat": None, "size": None},
+        }
+
+        self.updated = {"robot": False, "table": False}
+        self.prev_state = {"robot": None, "table": None}
+
+        self.latest_edge_text = None
+        self.edge_ready = False
+
+        # ===== 絶対エッジ番号（テーブル中心基準）=====
+        self.fixed_edges = {
+            "edge_1": np.array([0.75, 0.0]),
+            "edge_2": np.array([0.0, 0.45]),
+            "edge_3": np.array([-0.75, 0.0]),
+            "edge_4": np.array([0.0, -0.45]),
+        }
+
+        self.sub_robot = self.create_subscription(
+            Marker, "/robot_marker",
+            lambda msg: self.cb_marker(msg, "robot"), 10)
+
+        self.sub_table = self.create_subscription(
+            Marker, "/table_marker",
+            lambda msg: self.cb_marker(msg, "table"), 10)
+
+        self.pub_timer = self.create_timer(1.0, self.publish_edge)
+
+    # ------------------------------------------------
+    def is_changed(self, prev, curr, eps=1e-3):
+        if prev is None:
+            return True
+        return any(abs(p - c) > eps for p, c in zip(prev, curr))
+
+    def cb_marker(self, msg: Marker, name: str):
+        p, q, s = msg.pose.position, msg.pose.orientation, msg.scale
+        curr = (p.x, p.y, p.z, q.x, q.y, q.z, q.w, s.x, s.y, s.z)
+
+        if self.is_changed(self.prev_state[name], curr):
+            self.prev_state[name] = curr
+            self.updated[name] = True
+
+            self.state[name]["pos"] = (p.x, p.y, p.z)
+            self.state[name]["quat"] = (q.x, q.y, q.z, q.w)
+            self.state[name]["size"] = (s.x, s.y, s.z)
+
+            if self.updated["robot"] or self.updated["table"]:
+                self.calc_once()
+                self.updated["robot"] = False
+                self.updated["table"] = False
+
+    # ------------------------------------------------
+    def quat_to_yaw(self, qx, qy, qz, qw):
+        siny_cosp = 2.0 * (qw*qz + qx*qy)
+        cosy_cosp = 1.0 - 2.0 * (qy*qy + qz*qz)
+        return math.atan2(siny_cosp, cosy_cosp)
+
+    def have_all(self):
+        for obj in ["robot", "table"]:
+            for k in ["pos", "quat", "size"]:
+                if self.state[obj][k] is None:
+                    return False
+        return True
+
+    def calc_once(self):
+        if self.have_all():
+            self.PoseSelection()
+
+    # ------------------------------------------------
+    def PoseSelection(self):
+
+        # ===== 状態取得 =====
+        robot_x, robot_y, _ = self.state["robot"]["pos"]
+        table_x, table_y, _ = self.state["table"]["pos"]
+        sx, sy, _ = self.state["table"]["size"]
+        tqx, tqy, tqz, tqw = self.state["table"]["quat"]
+
+        yaw = self.quat_to_yaw(tqx, tqy, tqz, tqw)
+
+        hx, hy = sx / 2.0, sy / 2.0
+        c, s = math.cos(yaw), math.sin(yaw)
+
+        # ===== エッジ中心（local）=====
+        local_edges = {
+            "E0": np.array([+hx, 0.0]),
+            "E1": np.array([0.0, +hy]),
+            "E2": np.array([-hx, 0.0]),
+            "E3": np.array([0.0, -hy]),
+        }
+
+        local_corners = {
+                "C0":  np.array([+hx, -hy]),
+                "C1":  np.array([+hx, +hy]),
+                "C2":  np.array([-hx, +hy]),
+                "C3":  np.array([-hx, -hy]),
+                }
+
+        world_edges = {}
+        local_rotated = {}
+        local_rotated_corner = {}
+        local_rotated_edge = {}
+        corner_theta = {}
+        edge_theta = {}
+
+        # ===== ロボット→中心ベクトル =====
+        Pc = np.array([robot_x - table_x, robot_y - table_y])
+        angle_TR = np.degrees(np.arctan2(Pc[0], Pc[1]))
+
+        for name, (lx, ly) in local_edges.items():
+            ex = lx * c - ly * s
+            ey = lx * s - ly * c
+
+            local_rotated_edge[name] = np.array([ex, ey])
+        
+        for name, (lx, ly) in local_corners.items():
+            cx = lx * c - ly * s
+            cy = lx * s - ly * c
+
+            local_rotated_corner[name] = np.array([cx, cy])
+
+            angle_corner = np.degrees(np.arctan2(cx, cy))
+            theta = (angle_TR - angle_corner + 360.0) % 360.0
+            corner_theta[name] = theta
+
+
+        # ===== θでソート（右回り）=====
+        sorted_corner = sorted(corner_theta.items(), key=lambda x: x[1])
+        order = [e[0] for e in sorted_corner]
+        # エッジの名前順番入れ替え
+        original_corner_names = list(local_corners.keys())
+        sort_idx = [original_corner_names.index(name) for name in order]
+        edge_names = list(local_edges.keys())
+        edge_order = [edge_names[i] for i in sort_idx]
+
+        # ===== semanticラベル =====
+        THETA_A = 15.0
+
+        if sorted_corner[0][1] < THETA_A:
+            labels = ["Near left edge",
+                      "Near right edge",
+                      "Far right edge",
+                      "Far left edge"]
+        elif sorted_corner[3][1] < (360.0 - THETA_A):
+            labels = ["Near edge",
+                    "Right edge",
+                    "Far edge",
+                    "Left edge"]
+        else:
+            labels = ["Near right edge",
+                      "Far right edge",
+                      "Far left edge",
+                      "Near left edge"]
+
+        semantic_map = {}
+        for phys, label in zip(order, labels):
+            semantic_map[phys] = label
+
+        # =================================================
+        # 循環割当（修正版）
+        # =================================================
+
+        ref_phys = edge_order[0]
+
+        min_d = float("inf")
+        ref_edge_id = None
+        comp_edge = {} 
+
+        # ★ 修正：local_rotated で比較
+        for edge_id, fixed_pos in self.fixed_edges.items():
+            d = np.linalg.norm(local_rotated_edge[ref_phys] - fixed_pos)
+            if d < min_d:
+                min_d = d
+                ref_edge_id = edge_id
+
+        edge_ids = ["edge_1", "edge_2", "edge_3", "edge_4"]
+        ref_index = edge_ids.index(ref_edge_id)
+
+        assignment = {}
+
+        for i in range(4):
+            edge_id = edge_ids[(ref_index + i) % 4]
+            assignment[edge_id] = order[i]
+
+        # ===== 出力 =====
+        lines = []
+
+        self.get_logger().info("-----------------------------------")
+        for edge_id in edge_ids:
+            phys = assignment[edge_id]
+            sem = semantic_map[phys]
+            x, y = self.fixed_edges[edge_id]
+
+            line = f"{edge_id}: {sem} / ({x:.2f}, {y:.2f})"
+            lines.append(line)
+            self.get_logger().info(line)
+
+        self.latest_edge_text = "\n".join(lines)
+        self.edge_ready = True
+
+    # ------------------------------------------------
+    def publish_edge(self):
+        if self.edge_ready:
+            msg = String()
+            msg.data = self.latest_edge_text
+            self.edge_pub.publish(msg)
+
+
+def main():
+    rclpy.init()
+    node = EdgeLabeling()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ vint_ros/vint_ros/Perception_node.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+
+import json
+import vosk
+import sounddevice as sd
+import queue
+import threading
+import rclpy
+import requests
+import time
+import numpy as np
+import pyttsx3
+import math
+import random
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Empty
+from rclpy.node import Node
+from rclpy.parameter import Parameter as RclpyParameter
+from rclpy.executors import MultiThreadedExecutor
+from rcl_interfaces.msg import Parameter as MsgParameter, ParameterType, ParameterValue
+from rcl_interfaces.srv import SetParameters
+from vosk import Model, KaldiRecognizer , GpuInit
+from ament_index_python.packages import get_package_share_directory
+from geometry_msgs.msg import TransformStamped, PoseStamped
+from visualization_msgs.msg import Marker
+from tf2_ros import TransformBroadcaster
+from builtin_interfaces.msg import Duration
+from geometry_msgs.msg import Point
+from std_msgs.msg import String
+
+#---------------------------------------------------
+table_x = 2.5
+table_y = 1.5
+table_yaw_deg = 270.0
+table_name = "table"
+table_size_x = 1.5
+table_size_y = 0.9
+table_size_z = 0.75
+
+
+robot_x = 0.0
+robot_y = 0.0
+robot_yaw_deg = 0.0
+robot_name = "robot"
+robot_size_x = 0.5
+robot_size_y = 0.5
+robot_size_z = 1.0
+
+THETA_A = 15.0
+
+table_hsize_x = table_size_x / 2
+table_hsize_y = table_size_y / 2
+corners = [
+        (((table_x+table_hsize_x)*math.cos(table_yaw_deg)), ((table_y+table_hsize_y)*(-math.sin(table_yaw_deg))), table_size_z),
+        (((table_x-table_hsize_x)*math.cos(table_yaw_deg)), ((table_y+table_hsize_y)*(-math.sin(table_yaw_deg))), table_size_z),
+        (((table_x-table_hsize_x)*math.cos(table_yaw_deg)), ((table_y-table_hsize_y)*(-math.sin(table_yaw_deg))), table_size_z),
+        (((table_x+table_hsize_x)*math.cos(table_yaw_deg)), ((table_y-table_hsize_y)*(-math.sin(table_yaw_deg))), table_size_z)
+        ]
+
+robot_yaw_rad = math.radians(robot_yaw_deg)
+Robot = np.array([robot_x, robot_y, robot_yaw_deg, robot_yaw_rad, robot_name]) 
+table_yaw_rad = math.radians(table_yaw_deg)
+Table = np.array([table_x, table_y, table_yaw_deg, table_yaw_rad, table_name]) 
+
+#----------------------------------------------------
+class MapBroadcaster(Node):
+    def __init__(self):
+        super().__init__('map_broadcaster')
+        self.tf_broadcaster = TransformBroadcaster(self)
+        
+
+        #---- publish ----#
+        self.table_marker_pub = self.create_publisher(Marker, '/table_marker', 10)
+        self.robot_marker_pub = self.create_publisher(Marker, '/robot_marker', 10)
+        self.RtoT_marker_pub = self.create_publisher(Marker, '/RtoT_marker', 10)
+        self.Tcon_marker_pub = self.create_publisher(Marker, '/Tcon_marker', 10)
+        self.Redge_marker_pub = self.create_publisher(Marker, '/Redge_marker', 10)
+        self.robot_pose_pub = self.create_publisher(PoseStamped, '/robot_pose', 10)
+        self.marker = self.create_timer(1.0, self.publish_marker)        
+        #---- subscribe ----#
+        self.map_update_sub = self.create_subscription(
+                Float32MultiArray,
+                '/map_update',
+                self.map_update_cb,
+                10
+                )
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def yaw_to_quaternion(self, yaw):
+        half = yaw * 0.5
+        return (0.0, 0.0, math.sin(half), math.cos(half))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def map_update_cb(self, msg):
+        self.get_logger().info("map_update")
+        global table_x
+        global table_y
+        global table_yaw_deg
+        global table_yaw_rad
+        
+        table_x, table_y, table_yaw_deg, self.dummy_value = msg.data
+        table_yaw_rad = math.radians(table_yaw_deg)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def publish_marker(self):
+        now = self.get_clock().now().to_msg()
+        #---robot_TF---
+        q_robot = self.yaw_to_quaternion(robot_yaw_rad)
+        t_robot = TransformStamped()
+        t_robot.header.stamp = now
+        t_robot.header.frame_id = 'map'
+        t_robot.child_frame_id = 'robot_base'
+        t_robot.transform.translation.x = float(robot_x)
+        t_robot.transform.translation.y = float(robot_y)
+        t_robot.transform.translation.z = 0.0
+        t_robot.transform.rotation.x = q_robot[0]
+        t_robot.transform.rotation.y = q_robot[1]
+        t_robot.transform.rotation.z = q_robot[2]
+        t_robot.transform.rotation.w = q_robot[3]
+        self.tf_broadcaster.sendTransform(t_robot)
+
+        # ---table_TF---
+        q_table = self.yaw_to_quaternion(table_yaw_rad)
+        t_table = TransformStamped()
+        t_table.header.stamp = now
+        t_table.header.frame_id = 'map'
+        t_table.child_frame_id = 'table'
+        t_table.transform.translation.x = float(table_x)
+        t_table.transform.translation.y = float(table_y)
+        t_table.transform.translation.z = 0.0
+        t_table.transform.rotation.x = q_table[0]
+        t_table.transform.rotation.y = q_table[1]
+        t_table.transform.rotation.z = q_table[2]
+        t_table.transform.rotation.w = q_table[3]
+        self.tf_broadcaster.sendTransform(t_table)
+
+        # ---robot_PoseStamped---
+        pose = PoseStamped()
+        pose.header.stamp = now
+        pose.header.frame_id = 'map'
+        pose.pose.position.x = float(robot_x)
+        pose.pose.position.y = float(robot_y)
+        pose.pose.position.z = 0.0
+        pose.pose.orientation.x = q_robot[0]
+        pose.pose.orientation.y = q_robot[1]
+        pose.pose.orientation.z = q_robot[2]
+        pose.pose.orientation.w = q_robot[3]
+        self.robot_pose_pub.publish(pose)
+
+        #---table_Marker---
+        m_table = Marker()
+        m_table.header.stamp = now
+        m_table.header.frame_id = 'map'
+        m_table.ns = 'table'
+        m_table.id = 0
+        m_table.type = Marker.CUBE
+        m_table.action = Marker.ADD
+        # tabel_position
+        table_height = table_size_z
+        m_table.pose.position.x = float(table_x)
+        m_table.pose.position.y = float(table_y)
+        m_table.pose.position.z = table_height / 2.0
+        m_table.pose.orientation.x = q_table[0]
+        m_table.pose.orientation.y = q_table[1]
+        m_table.pose.orientation.z = q_table[2]
+        m_table.pose.orientation.w = q_table[3]
+        # table_size
+        m_table.scale.x = table_size_x
+        m_table.scale.y = table_size_y
+        m_table.scale.z = table_height
+        m_table.color.r = 0.0
+        m_table.color.g = 0.0
+        m_table.color.b = 1.0
+        m_table.color.a = 0.5
+        m_table.lifetime = Duration(sec=0, nanosec=0)
+        self.table_marker_pub.publish(m_table)
+
+        # ---robot_Marker---
+        m_robot = Marker()
+        m_robot.header.stamp = now
+        m_robot.header.frame_id = 'map'
+        m_robot.ns = 'robot'
+        m_robot.id = 1
+        m_robot.type = Marker.CUBE
+        m_robot.action = Marker.ADD
+        # robot_position
+        robot_height = robot_size_z
+        m_robot.pose.position.x = float(robot_x)
+        m_robot.pose.position.y = float(robot_y)
+        m_robot.pose.position.z = robot_height / 2.0
+        # robot_orientation
+        m_robot.pose.orientation.x = q_robot[0]
+        m_robot.pose.orientation.y = q_robot[1]
+        m_robot.pose.orientation.z = q_robot[2]
+        m_robot.pose.orientation.w = q_robot[3]
+        # robot_size
+        m_robot.scale.x = robot_size_x
+        m_robot.scale.y = robot_size_y
+        m_robot.scale.z = robot_height
+        m_robot.color.r = 0.0
+        m_robot.color.g = 1.0
+        m_robot.color.b = 0.0
+        m_robot.color.a = 1.0
+        m_robot.lifetime = Duration(sec=0, nanosec=0)
+        self.robot_marker_pub.publish(m_robot)
+        
+        # ---vector_P_ijkl---
+        m_Tcon = Marker()
+        m_Tcon.header.stamp = now
+        m_Tcon.header.frame_id = "map"
+        m_Tcon.ns = "line"
+        m_Tcon.id = 2
+        m_Tcon.type = Marker.LINE_LIST
+        m_Tcon.action = Marker.ADD
+        m_Tcon.scale.x = 0.02
+        m_Tcon.color.r = 0.8
+        m_Tcon.color.g = 0.3
+        m_Tcon.color.b = 0.0
+        m_Tcon.color.a = 1.0
+        for corner in corners:
+            start = Point()
+            start.x, start.y, start.z = table_x, table_y, (table_size_z)
+            end = Point()
+            end.x, end.y, end.z = corner
+            m_Tcon.points.extend([start, end])
+        m_Tcon.lifetime = Duration(sec=0, nanosec=0)
+        self.Tcon_marker_pub.publish(m_Tcon)
+
+        # ---vector_RtoT---
+        m_RtoT = Marker()
+        m_RtoT.header.stamp = now
+        m_RtoT.header.frame_id = "map"
+        m_RtoT.ns = "line"
+        m_RtoT.id = 2
+        m_RtoT.type = Marker.LINE_LIST
+        m_RtoT.action = Marker.ADD
+        m_RtoT.scale.x = 0.02
+        m_RtoT.color.r = 0.0
+        m_RtoT.color.g = 0.8
+        m_RtoT.color.b = 0.3
+        m_RtoT.color.a = 1.0
+        start_point_RtoT = Point()
+        start_point_RtoT.x = robot_x
+        start_point_RtoT.y = robot_y
+        start_point_RtoT.z = robot_size_z
+        end_point_RtoT = Point()
+        end_point_RtoT.x = table_x
+        end_point_RtoT.y = table_y
+        end_point_RtoT.z = table_size_z
+        m_RtoT.points.append(start_point_RtoT)
+        m_RtoT.points.append(end_point_RtoT)
+        m_RtoT.lifetime = Duration(sec=0, nanosec=0)
+        self.RtoT_marker_pub.publish(m_RtoT)
+
+def main():
+    rclpy.init()
+    broadcaster = MapBroadcaster()
+    rclpy.spin(broadcaster)
+
+if __name__=="__main__":
+    main()
