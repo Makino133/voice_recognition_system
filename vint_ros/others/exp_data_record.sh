815ca9dc7d898905082bd62bc28c3b3e2aa15bcb
SantiagoPrice <santiago.price.torrendell@gmail.com>
2026-03-02 12:59:35 +0900

    Freeze tmarker after command

---
+++ vint_ros/others/exp_data_record.sh
@@ -0,0 +1,2 @@
+# Command for storing topic for the real world exp
+ros2 bag record -o data2.bag -b 2147483648 /global_costmap/published_footprint /table_marker /map /plan /tf /goal_pose /cand_poses /command /LLM_out
--- vint_ros/vint_ros/Goal_to_Nav2_node.py
+++ vint_ros/vint_ros/Goal_to_Nav2_node.py
@@ -7,6 +7,8 @@ from nav2_msgs.action import NavigateToPose
 from geometry_msgs.msg import PoseStamped
 from rclpy.action import ActionClient
 from action_msgs.srv import CancelGoal
+from sensor_msgs.msg import PointCloud2
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
 
 class GOALtoNAV2(Node):
@@ -14,14 +16,30 @@ class GOALtoNAV2(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
 
-        self._goal_handle = None
+        self._goal_handle_ = None
+        self.is_command_sent_ = False
+        self.mrk= Marker()
 
-        self.subscription = self.create_subscription(
-            Marker,
-            "/select_poses",
-            self.send_goal,
-            10
-        )
+        #------------------------------------------------------------
+        # Subscribers
+        #------------------------------------------------------------
+
+        self.create_subscription(Marker,"/select_poses",self.send_goal,10)
+
+        qos_pc = QoSProfile( reliability=QoSReliabilityPolicy.BEST_EFFORT,history=QoSHistoryPolicy.KEEP_LAST,depth=1) # ensuring the newest message is read
+        self.create_subscription(PointCloud2, '/lidar_RAM_segm', self.plane_cloud_callback, qos_pc)
+
+        self.create_subscription(Marker, "/table_marker_rel",lambda msg:self.cb_marker(msg,"table"), 10)
+        self.create_subscription(Marker, "/robot_marker_rel",lambda msg:self.cb_marker(msg,"robot"), 10)
+
+
+
+        #------------------------------------------------------------
+        # Publishers
+        #------------------------------------------------------------
+        self.pc_pub = self.create_publisher(PointCloud2, '/lidar_RAM_segm_rel',1)
+        self.tmrk_pub = self.create_publisher(Marker, '/table_marker',1)
+        self.rmrk_pub = self.create_publisher(Marker, '/robot_marker',1)
     
         #------------------------------------------------------------
         # Action client for NavigateToPose
@@ -47,14 +65,20 @@ class GOALtoNAV2(Node):
 
             # Sending message
 
-            if self._goal_handle:
+            if self._goal_handle_:
                 self.get_logger().info('Attempting to cancel the current goal...')
-                cancel_future = self._goal_handle.cancel_goal_async()           
+                cancel_future = self._goal_handle_.cancel_goal_async()           
                 cancel_future.add_done_callback(lambda fut: self.cancel_done_callback(fut, g_msg))
             
             else:
                 self.get_logger().info('Attempting to send goal')
                 if g_msg:
+
+                    #Publishing the last marker in red to signal the table stops updating
+                    self.is_command_sent_= True
+                    self.mrk.color = type(self.mrk.color)(r=1.0, g=0.0, b=0.0, a=1.0)
+                    self.tmrk_pub.publish(self.mrk)
+
                     future = self.navigate_to_pose_client.send_goal_async(g_msg)
                     future.add_done_callback(lambda fut: self.goal_response_callback)
                 else:
@@ -65,7 +89,7 @@ class GOALtoNAV2(Node):
         result = fut.result()
         if len(result.goals_canceling) > 0:
             self.get_logger().info('Goal successfully cancelled.')
-            self._goal_handle = None
+            self._goal_handle_ = None
             if g_msg:
                 future = self.navigate_to_pose_client.send_goal_async(g_msg)
                 future.add_done_callback(self.goal_response_callback)
@@ -81,8 +105,26 @@ class GOALtoNAV2(Node):
                 self.get_logger().warn('Goal was rejected.')
                 return
 
-            self._goal_handle = goal_handle
+            self._goal_handle_ = goal_handle
             self.get_logger().info('Goal accepted and executing.')
+
+    def plane_cloud_callback(self, plane_cloud_msg):
+        if not self.is_command_sent_:
+              self.pc_pub.publish(plane_cloud_msg)
+
+    def cb_marker(self, msg: Marker, label):
+         
+         if label is "robot":
+              mrk_pub=self.rmrk_pub
+         else:
+              mrk_pub=self.tmrk_pub
+              self.mrk=msg
+         
+         if not self.is_command_sent_:
+              mrk_pub.publish(msg)
+              
+
+        
 
 
 
