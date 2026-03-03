4bf193821845bbcc03b57bd179adba51cb011937
SantiagoPrice <santiago.price.torrendell@gmail.com>
2026-03-03 20:08:13 +0900

    Remove prescribed filename

--- vint_ros/others/exp_data_record.sh
+++ vint_ros/others/exp_data_record.sh
@@ -1,2 +1,2 @@
 # Command for storing topic for the real world exp
-ros2 bag record -o data2.bag -b 2147483648 /global_costmap/published_footprint /table_marker /map /plan /tf /goal_pose /cand_poses /command /LLM_out
+ros2 bag record -b 2147483648 /global_costmap/published_footprint /table_marker /map /plan /tf /select_poses /cand_poses /command /LLM_out /global_costmap/costmap
